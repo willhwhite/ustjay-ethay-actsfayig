57557539dce07f39c5a8ba2a8f784309235f6f03
Will <williamwhite0@gmail.com>
2019-04-22 22:24:47 -0700

    Initial commit.

--- main.py
+++ main.py
@@ -17,12 +17,30 @@ def get_fact():
     return facts[0].getText()
 
 
+def get_pig_latin(fact):
+    url = "https://hidden-journey-62459.herokuapp.com/piglatinize/"
+    payload = {"input_text": fact}
+
+    response = requests.post(url, data=payload, allow_redirects=False)
+    url = response.headers['Location']
+    # response = requests.get("{}".format(url))
+    #
+    # soup = BeautifulSoup(response.content, "html.parser")
+    # facts = soup.find("body")
+
+    # return facts.getText()
+
+    return url
+
+
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact()
+    response = get_pig_latin(fact)
+
+    return response
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
